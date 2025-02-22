2320d809cabbb1fccd48d4b74ebe19fe6bed3c2e
Pierre Hofmann <pierreh2004@gmail.com>
2025-02-22 20:46:31 +0100

    rm lines

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors());
-
   app.use(helmet());
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new TransformationInterceptor(app.get(Reflector)));
-
   await app.listen(process.env.DEV_PORT ?? 3000, '0.0.0.0');
 }
-
 bootstrap();
