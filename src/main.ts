0b7d94af34df9fa0eafd3501a628130d9f0d13a7
Pierre Hofmann <pierreh2004@gmail.com>
2025-02-22 20:12:44 +0100

    cs

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { TransformationInterceptor } from './interceptor/response.interceptor';
-import * as cors from 'cors';  // Import du module cors
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Utilisation de CORS
-  app.use(cors());  // Activer CORS pour toutes les requêtes (tu peux personnaliser plus tard si besoin)
+  app.use(cors());
 
   app.use(helmet());
   app.setGlobalPrefix('api');
@@ -17,7 +16,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new TransformationInterceptor(app.get(Reflector)));
 
-  // Lancer l'application sur le port
   await app.listen(process.env.DEV_PORT ?? 3000, '0.0.0.0');
 }
 
