59f39bda2b9acf364d5e41c4786da3ae412f1702
Pierre Hofmann <pierreh2004@gmail.com>
2025-02-22 19:32:07 +0100

    (fix) ip for expo go

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,22 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { TransformationInterceptor } from './interceptor/response.interceptor';
+import * as cors from 'cors';  // Import du module cors
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Utilisation de CORS
+  app.use(cors());  // Activer CORS pour toutes les requêtes (tu peux personnaliser plus tard si besoin)
+
   app.use(helmet());
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new TransformationInterceptor(app.get(Reflector)));
-  await app.listen(process.env.DEV_PORT ?? 3000);
+
+  // Lancer l'application sur le port
+  await app.listen(process.env.DEV_PORT ?? 3000, '0.0.0.0');
 }
+
 bootstrap();
